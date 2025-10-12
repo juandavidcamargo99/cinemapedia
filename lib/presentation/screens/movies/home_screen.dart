bbecea3d750e051349a082426c5a892ce64d094f
juan david camargo <juandavidcamargo99@gmail.com>
2025-10-12 00:50:29 -0500

    configuration enviroments variables

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,7 +1,7 @@
+import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  
   static const String name = 'home-screen';
 
   const HomeScreen({super.key});
@@ -9,10 +9,8 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
-      ),
-      body: const Placeholder(),
+      appBar: AppBar(title: const Text('Home Screen')),
+      body: Center(child: Text(Environment.theMovieDbKey)),
     );
   }
 }
